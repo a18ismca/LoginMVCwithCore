4fdb68cae608f94bf91ab64d13606574756cdab6
a18ismca <a18ismca@student.his.se>
2021-12-19 22:10:52 +0100

    Connected to a MySQL database.

---
+++ LoginAppMVC/LoginAppMVC/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LoginAppMVC.Models;
+
+namespace LoginAppMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        // GET: Login/LoginView
+        public IActionResult LoginView()
+        {
+            var loginConnection = new LoginConnect();
+            ViewBag.LoginTable = loginConnection.GetAllUsers();
+            return View();
+        }
+    }
+}
---
+++ LoginAppMVC/LoginAppMVC/Models/LoginConnect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace LoginAppMVC.Models
+{
+    public class LoginConnect
+    {
+        private readonly string ConnectionString = "Server=localhost;Database=ismetsdb;User ID=root;Password=Pridvorci99;Pooling=false;SslMode=none;convert zero datetime=True;";
+
+
+        public DataTable GetAllUsers() {
+            MySqlConnection dbcon = new MySqlConnection(ConnectionString);
+            dbcon.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("select * from users;", dbcon);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable userTable = ds.Tables["result"];
+            dbcon.Close();
+
+            return userTable;
+
+        }
+    }
+}
